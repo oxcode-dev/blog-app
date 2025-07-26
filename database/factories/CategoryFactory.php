020f8c5f6261af16052a8647a5c0e51f2564587d
Oxcode <samuel@tedbree.com>
2025-07-26 15:42:53 +0100

    initial commit

--- database/factories/CategoryFactory.php
+++ database/factories/CategoryFactory.php
@@ -3,12 +3,16 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Category>
  */
 class CategoryFactory extends Factory
 {
+    protected $categories = [
+        'sport', 'global news', 'business', 'local news',
+    ];
     /**
      * Define the model's default state.
      *
@@ -17,7 +21,10 @@ class CategoryFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'id' => fake()->uuid(),
+            'name' => $category = fake()->randomElement($this->categories),
+            'slug' => Str::slug($category) . '-' . Str::strtotime(now()),
+            'description' => fake()->realText($maxNbChars = 500, $indexSize = 2),
         ];
     }
 }
