30ede2ea3226c666d3eb65e32d37d473a8a7c535
Oxcode <samuel@tedbree.com>
2025-07-27 14:44:53 +0100

    initial commit

--- routes/api.php
+++ routes/api.php
@@ -10,6 +10,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
+    
     return $request->user();
 })->middleware('auth:sanctum');
 
