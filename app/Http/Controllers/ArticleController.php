858210489225dbd06098a9213f5a02709bcae214
Oxcode <samuel@tedbree.com>
2025-08-01 23:02:55 +0100

    initial commit

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -3,8 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Inertia\Inertia;
 
 class ArticleController extends Controller
 {
-    
+    public function edit(Request $request)//: Response
+    {
+        return Inertia::render('settings/Profile', [
+            'status' => $request->session()->get('status'),
+        ]);
+    }
 }
