62fda7ba1909388de863a7b81d97d53aa9bfc6a0
Oxcode <samuel@tedbree.com>
2025-08-02 13:04:30 +0100

    initial commit

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -10,9 +10,9 @@ class ArticleController extends Controller
     public function index(Request $request)//: Response
     {
 
-        dd('Hello World');
-        // return Inertia::render('Articles', [
-        //     'status' => $request->session()->get('status'),
-        // ]);
+        // dd('Hello World');
+        return Inertia::render('Articles', [
+            'status' => $request->session()->get('status'),
+        ]);
     }
 }
