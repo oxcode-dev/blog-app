4d53ab24ffd8c48bd27a5fde540e84f603f5f029
Oxcode <samuel@tedbree.com>
2025-08-02 17:09:25 +0100

    initial commit

--- resources/js/pages/articles/show.vue
+++ resources/js/pages/articles/show.vue
@@ -26,6 +26,16 @@ const article = computed(() => usePage().props?.article || {})
                 <pre>{{ article }}</pre>
 
             </div>
+            <div class="bg-white shadow-sm overflow-hidden sm:rounded-lg my-4">
+                <div class="border-t border-gray-200 px-4 py-5 sm:p-0">
+                    <dl class="sm:divide-y sm:divide-gray-200">
+                        <div class="py-4 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                            <dt class="text-sm font-medium text-gray-500">First name</dt>
+                            <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">Mohamed</dd>
+                        </div>
+                    </dl>
+                </div>
+            </div>
         </div>
     </AppLayout>
 </template>
