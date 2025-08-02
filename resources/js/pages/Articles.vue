453a95c04648fb78be01b85dc98998fb35b33585
Oxcode <samuel@tedbree.com>
2025-08-02 14:43:15 +0100

    initial commit

--- resources/js/pages/Articles.vue
+++ resources/js/pages/Articles.vue
@@ -12,6 +12,8 @@ const breadcrumbs: BreadcrumbItem[] = [
 ];
 
 const articles = computed(() => usePage().props?.articles || {})
+const articlesData = computed(() => usePage().props?.articles?.data || [])
+
 </script>
 
 <template>
@@ -20,9 +22,33 @@ const articles = computed(() => usePage().props?.articles || {})
     <AppLayout :breadcrumbs="breadcrumbs">
         <div class="flex h-full flex-1 flex-col gap-4 rounded-xl p-4 overflow-x-auto">
             <div class="relative min-h-[100vh] rounded-xl border border-sidebar-border/70 md:min-h-min dark:border-sidebar-border">
-                <pre>
-                    {{ articles }}
-                </pre>
+
+                <div>
+                    <table class="min-w-full bg-white shadow-md rounded-xl">
+                        <thead>
+                            <tr class="bg-blue-gray-100 text-gray-700 border-b">
+                            <th class="py-3 px-4 text-left">Stock Name</th>
+                            <th class="py-3 px-4 text-left">Price</th>
+                            <th class="py-3 px-4 text-left">Quantity</th>
+                            <th class="py-3 px-4 text-left">Total</th>
+                            <th class="py-3 px-4 text-left">Action</th>
+                            </tr>
+                        </thead>
+                        <tbody class="text-blue-gray-900">
+                            <tr class="border-b border-blue-gray-200">
+                            <td class="py-3 px-4">Company A</td>
+                            <td class="py-3 px-4">$50.25</td>
+                            <td class="py-3 px-4">100</td>
+                            <td class="py-3 px-4">$5025.00</td>
+                            <td class="py-3 px-4">
+                                <a href="#" class="font-medium text-blue-600 hover:text-blue-800">Edit</a>
+                            </td>
+                            </tr>
+                            
+                        
+                        </tbody>
+                    </table>
+                </div>
             </div>
         </div>
     </AppLayout>
