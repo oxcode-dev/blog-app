1bda837b447395ea2f95db734d2ad2cc4d9e781a
Oxcode <samuel@tedbree.com>
2025-08-02 13:39:15 +0100

    initial commit

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Article;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 
@@ -9,8 +10,15 @@ class ArticleController extends Controller
 {
     public function index(Request $request)//: Response
     {
+        $articles = Article::search($request->get('search', ''))
+            ->orderBy(
+                $request->get('sortField', 'created_at'),
+                $request->get('sortAsc') === 'true' ? 'asc' : 'desc'
+            )    
+            ->paginate($request->get('perPage', 5));
         return Inertia::render('Articles', [
             'status' => $request->session()->get('status'),
+            'articles' => $articles,
         ]);
     }
 }
--- resources/js/pages/Articles.vue
+++ resources/js/pages/Articles.vue
@@ -1,8 +1,8 @@
 <script setup lang="ts">
 import AppLayout from '@/layouts/AppLayout.vue';
 import { type BreadcrumbItem } from '@/types';
-import { Head } from '@inertiajs/vue3';
-import PlaceholderPattern from '../components/PlaceholderPattern.vue';
+import { Head, usePage } from '@inertiajs/vue3';
+import { computed } from 'vue';
 
 const breadcrumbs: BreadcrumbItem[] = [
     {
@@ -10,6 +10,8 @@ const breadcrumbs: BreadcrumbItem[] = [
         href: '/articles',
     },
 ];
+
+const articles = computed(() => usePage().props?.articles || {})
 </script>
 
 <template>
@@ -17,8 +19,10 @@ const breadcrumbs: BreadcrumbItem[] = [
 
     <AppLayout :breadcrumbs="breadcrumbs">
         <div class="flex h-full flex-1 flex-col gap-4 rounded-xl p-4 overflow-x-auto">
-            <div class="relative min-h-[100vh] flex-1 rounded-xl border border-sidebar-border/70 md:min-h-min dark:border-sidebar-border">
-                <PlaceholderPattern />
+            <div class="relative min-h-[100vh] rounded-xl border border-sidebar-border/70 md:min-h-min dark:border-sidebar-border">
+                <pre>
+                    {{ articles }}
+                </pre>
             </div>
         </div>
     </AppLayout>
