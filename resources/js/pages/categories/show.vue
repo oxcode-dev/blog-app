3aae5a858c1c91811f92d59c97339e669eebe009
Oxcode <samuel@tedbree.com>
2025-08-03 17:44:07 +0100

    initial commit

--- resources/js/pages/categories/show.vue
+++ resources/js/pages/categories/show.vue
@@ -59,7 +59,7 @@ const handleDeleteCategory = () => {
                             <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">{{ category?.description || '' }}</dd>
                         </div>
                         <div class="py-4 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-                            <dt class="text-sm font-medium text-gray-500">Posted Date</dt>
+                            <dt class="text-sm font-medium text-gray-500">Created Date</dt>
                             <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">{{ category?.created_at || '' }}</dd>
                         </div>
                     </dl>
